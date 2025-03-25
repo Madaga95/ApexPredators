3c5d1a96e81b35102e0cab7980d696773492dc50
Madaga95 <amathgueye95@gmail.com>
2025-03-25 13:25:15 +0100

    Initial Commit

---
+++ ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Madaga on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
