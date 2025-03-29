2a1e4193e56b5a7982e2eb3de2181507d23eb4e9
Madaga95 <amathgueye95@gmail.com>
2025-03-29 22:27:29 +0100

    Initialisation

---
+++ ApexPredators/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  ApexPredators
+//
+//  Created by Madaga on 27/03/2025.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
---
+++ ApexPredators/PredatorMap.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorMap.swift
+//  ApexPredators
+//
+//  Created by Madaga on 29/03/2025.
+//
+
+import SwiftUI
+
+struct PredatorMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorMap()
+}
